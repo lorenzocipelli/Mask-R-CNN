b6fd866f0f7d926d17a0fdf407030c769804328b
Lorenzo Cipelli <95569843+lorenzocipelli@users.noreply.github.com>
2024-02-06 16:35:56 +0100

    model definition

--- testing.py
+++ testing.py
@@ -1,14 +1,37 @@
 import json
 import os
-
+import torchvision
 import matplotlib.pyplot as plt
-from torchvision.io import read_image
 
+from torchvision.io import read_image
 from utils.modanet_dataset import ModaNetDataset
 from utils.utils import get_transform
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 print('getcwd:      ', os.getcwd())
 
-modanet = ModaNetDataset("dataset", get_transform())
+#modanet = ModaNetDataset("dataset", get_transform())
 
-modanet[50]
+def get_mask_rcnn_model(num_classes) :
+    # load an instance segmentation model pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(
+        in_features_mask,
+        hidden_layer,
+        num_classes
+    )
+
+    return model
+
+get_mask_rcnn_model(num_classes=14) # with background
