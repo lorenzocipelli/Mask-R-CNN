4a39095d8e38662df5094c57726c60ad40a5ca83
Lorenzo Cipelli <95569843+lorenzocipelli@users.noreply.github.com>
2024-02-07 18:22:00 +0100

    comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torchvision
 import argparse
 import numpy as np
 import torch
+import torch.optim as optim
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
@@ -19,6 +20,8 @@ from utils.utils import get_transform
 
 FIRST_STOP = 32728 # 70 %
 SECOND_STOP = 7013 # 15 %
+NUM_EPOCHS = 10
+DEVICE = torch.device("cpu")
 
 def get_subsets(dataset) :
     # total -> 46754 elements (100 %)
@@ -45,14 +48,34 @@ def main() :
     #valid_loader = DataLoader(val_modanet, batch_size=16, shuffle=False, num_workers=4)
     #test_loader = DataLoader(test_modanet, batch_size=16, shuffle=False, num_workers=4)
 
-    for i, data in enumerate(train_loader):
-        print("Indice Enumerate: " + str(i))
-        #print("New target: ")
-        #print(target) 
+    model = MaskRCNN(num_classes=14)
+
+    optimizer = optim.SGD(model.parameters(), lr=1e-5)
+
+    model.train()
+
+    for epoch in range(NUM_EPOCHS):
+        prog_bar = tqdm(train_loader, total=len(train_loader))
+
+        for i, data in enumerate(prog_bar):
+            images, target = data
+            # from .....torchvision_tutorial.html
+            images =  list(image.to(DEVICE) for image in images)
+            targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
+
+            optimizer.zero_grad()
+
+            output = model(images, targets)
+            # capire il criterion da utilizzare
+            #loss = criterion(outputs, labels)
+            #loss.backward()
+            #optimizer.step()
+            
+            print(output)
 
     print("Training Finished !")
 
-    #model = MaskRCNN(num_classes=14)
+
 
 if __name__ == "__main__" :
     main()
